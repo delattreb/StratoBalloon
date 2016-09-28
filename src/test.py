d5253400a6ba78c0671f38d7e044211a902822c8
Bruno <delattreb@gmail.com>
2016-09-28 15:14:20 +0200

    Add SR04 sensor

--- src/test.py
+++ src/test.py
@@ -1,46 +1,20 @@
-# message = "Chuc"
-message = "Chuck "
 
-bin_message = ""
-cpt = 0
-for i in message:
-    binary = bin(ord(i))[2:]
 
-    if cpt > 1:
-        if len(binary) < 8:
-            for j in range(8 - len(binary)):
-                binary = "0" + binary
 
-    bin_message += binary
-    cpt += 1
+def secu():
+    get eau
+    get tasse
 
-print(bin_message)
 
-# check serie
-serie = 0
-last = bin_message[0]
-out = ""
-cpt = 0
-for i in bin_message:
-    cpt += 1
-    if i == last:
-        serie += 1
-    else:
-        if last == "0":
-            out += "00 "
-        else:
-            out += "0 "
-        for j in range(serie):
-            out += "0"
-        out += " "
-        serie = 1
-    last = i
-    if cpt == len(bin_message):
-        if last == "0":
-            out += "00 "
-        else:
-            out += "0 "
-        for j in range(serie):
-            out += "0"
+if secu()
+    iftt
 
-print(out)
+if iftt
+    while (secu())
+        chauffer eau
+        if eau chaude
+            lancer café
+
+    eteindre eau
+fin
+
