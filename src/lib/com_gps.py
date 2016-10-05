a3c443c13895ef15f8985f52c298f970e2ca37b4
Bruno <delattreb@gmail.com>
2016-10-05 14:19:49 +0200

    Add GPS gesture

--- src/lib/com_gps.py
+++ src/lib/com_gps.py
@@ -1,1 +1,41 @@
-http://pibot-maison.blogspot.fr/2016/01/installer-un-gps-sur-raspberry-pi.html
+"""
+com_gps.py v1.0.0
+Auteur: Bruno DELATTRE
+Date : 04/10/2016
+"""
+
+import os
+
+from gps import *
+
+
+class GPS:
+    def __init__(self):
+        self.latitude = 0
+        self.longiture = 0
+        self.timeutc = 0
+        self.altitude = 0
+        self.speed = 0
+        self.satellites = 0
+
+    def getLocalisation(self):
+        session = gps(mode=WATCH_ENABLE)
+        try:
+            while True:
+                donnees = session.next()
+                if donnees['class'] == "TPV":
+                    os.system('clear')
+                    self.latitude = session.fix.latitude
+                    self.longitude = session.fix.longitude
+                    self.timeutc = session.utc
+                    self.altitude = session.fix.altitude
+                    self.speed = session.fix.speed
+                    self.satellites = session.satellites
+        except KeyError:
+            pass
+        except KeyboardInterrupt:
+            print("closed by user")
+        except StopIteration:
+            print("GPSD off")
+        finally:
+            session = None
