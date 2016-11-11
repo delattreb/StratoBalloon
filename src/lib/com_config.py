8332e4fb0f1d588dd389987451566e810626a4ff
Bruno <delattreb@gmail.com>
2016-11-11 09:00:26 +0100

    Resolve before merge

--- src/lib/com_config.py
+++ src/lib/com_config.py
@@ -12,21 +12,21 @@ config_file = "config/config.ini"
 
 def setConfig():
     config = configparser.ConfigParser()
-
+    
     # region Config
     # Version
     config['VERSION'] = {}
     config['VERSION']['last'] = '1.0.1'
-
+    
     # LOGGER
     config['LOGGER'] = {}
     config['LOGGER']['level'] = '10'
     # Info: DEBUG=10 INFO= 20 WARNING=30 ERROR=40 #CRITICAL=50
-
+    
     # SQLite
     config['SQLITE'] = {}
     config['SQLITE']['database'] = 'database.db'
-
+    
     # Camera
     config['CAMERA'] = {}
     config['CAMERA']['pic_resolution_x'] = '3280'
@@ -41,33 +41,46 @@ def setConfig():
     config['CAMERA']['exposure_mode'] = ''
     config['CAMERA']['delay'] = '5'
     config['CAMERA']['nb'] = '3'
-
+    
     # GPIO
     config['GPIO'] = {}
+    
     # DHT11 interior
     config['GPIO']['DHT11_INTERIOR_PORT'] = '24'
     config['GPIO']['DHT11_INTERIOR_delay'] = '15'
     config['GPIO']['DHT11_INTERIOR_nb'] = '30'
+    
     # DHT11 exterior
     config['GPIO']['DHT11_EXTERIOR_PORT'] = '25'
     config['GPIO']['DHT11_EXTERIOR_delay'] = '15'
     config['GPIO']['DHT11_EXTERIOR_nb'] = '30'
-
+    
+    # DHT22
+    config['GPIO']['DHT22_INTERIOR_PORT'] = '5'
+    config['GPIO']['DHT22_INTERIOR_delay'] = '3'
+    config['GPIO']['DHT22_INTERIOR_nb'] = '30'
+    
+    # DS18B20
+    config['GPIO']['DS18B20_1'] = '/sys/bus/w1/devices/w1_bus_master1/28-0416618c01ff/w1_slave'
+    config['GPIO']['DS18B20_1_delay'] = '5'
+    config['GPIO']['DS18B20_1_nb'] = '60'
+    
     # SR04
     config['GPIO']['SR04_triger_port'] = '17'
     config['GPIO']['SR04_echo_port'] = '27'
     config['GPIO']['SR04_delay'] = '1'
     config['GPIO']['SR04_nb'] = '60'
-
-    # DS18B20
-    config['GPIO']['DS18B20_1'] = '/sys/bus/w1/devices/w1_bus_master1/28-0416618c01ff/w1_slave'
-    config['GPIO']['DS18B20_1_delay'] = '5'
-    config['GPIO']['DS18B20_1_nb'] = '60'
+    
+    # GPS
+    config['GPS'] = {}
+    config['GPS']['delay'] = '0.5'
+    config['GPS']['nb'] = '3000'
+    
     # Directory
     config['DIRECTORY'] = {}
     config['DIRECTORY']['picture_path'] = '/home/pi/'
     # endregion
-
+    
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     db_path = os.path.join(BASE_DIR, config_file)
     with open(db_path, 'w') as configfile:
